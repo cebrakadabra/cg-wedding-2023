92835a5006fff5330df93fc1c41f7805f1f7e1dd
cebrakadabra <admin@relax-tax.at>
2022-11-13 16:45:40 -0800

    us schedule update

--- components/Events.js
+++ components/Events.js
@@ -30,19 +30,9 @@ export default function Events({locale, desktopAsset, enableYear, currentYear}) 
                                         <div className="icon-round mb-5 mb-md-3  bg-icon-primary">
                                             <h2 style={{margin: 0}}>🍕</h2>
                                         </div>
-                                        <h5 className="mb-0"><strong>Welcome Dinner</strong></h5>
-                                        {currentYear === enableYear ? (
-                                            <>
-                                                <h6 className="mb-5">6:00pm @Flying Caballos</h6>
-                                                <p className="text-align-center">Please tell us if you can make it</p>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <p className="text-align-center">More details to follow</p>
-                                            </>
-                                        )
-                                        }
-
+                                        <h5 className="mb-0"><strong>Rehearsal Dinner</strong></h5>
+                                        <h6 className="mb-5" style={{fontSize: '12px', color: '#787878', }}><strong>When:</strong> 6:30pm - Flying Caballos Ranch <br/><strong>Where:</strong> 1150 Farmhouse Ln, San Luis Obispo</h6>
+                                        <p className="text-align-center" style={{fontSize: '12px', color: '#787878', }}><strong>Who:</strong> Exclusively for the Bridal Party (Bridesmaids & Groomsmen) and close family members</p>
                                     </>
                                 </>
                             ) : locale === 'at' ? (
@@ -54,8 +44,8 @@ export default function Events({locale, desktopAsset, enableYear, currentYear}) 
                                             <h2 style={{margin: 0}}>🍕</h2>
                                         </div>
                                         <h5 className="mb-0"><strong>Welcome Dinner</strong></h5>
-                                            <h6 className="mb-5" style={{fontSize: '12px', color: '#787878', }}><strong>When:</strong> 6:00pm - Hotel Eder <br/><strong>Where:</strong> Am Dorfpl. 5, Maria Alm</h6>
-                                            <p className="text-align-center" style={{fontSize: '12px', color: '#787878', }}><strong>Who:</strong> Exclusively for the Bridal Party (Bridesmaids & Groomsmen) and for those who travel from far and close family members</p>
+                                        <h6 className="mb-5" style={{fontSize: '12px', color: '#787878', }}><strong>When:</strong> 6:00pm - Hotel Eder <br/><strong>Where:</strong> Am Dorfpl. 5, Maria Alm</h6>
+                                        <p className="text-align-center" style={{fontSize: '12px', color: '#787878', }}><strong>Who:</strong> Exclusively for the Bridal Party (Bridesmaids & Groomsmen), for those who travel from far and close family members</p>
                                     </>
                                 </>
                             ) : null}
@@ -72,17 +62,15 @@ export default function Events({locale, desktopAsset, enableYear, currentYear}) 
                                             <h2 style={{margin: 0}}>💍</h2>
                                         </div>
                                         <h5 className="mb-0 text-white"><strong>Wedding Ceremony</strong></h5>
-                                        {currentYear === enableYear ? (
-                                            <>
-                                                <h6 className="mb-5 text-white">3:30PM @Flying Caballos</h6>
-                                                <p className="text-align-center">Please be seated by 04:00pm</p>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <p className="text-align-center">More details to follow</p>
-                                            </>
-                                        )
-                                        }
+                                        <h6 className="mb-5 text-white" style={{fontSize: '12px', color: '#ffffff' }}>
+                                            <strong>When: </strong> 4:00PM - Flying Caballos Ranch <br/>
+                                            <strong>Where: </strong>1150 Farmhouse Ln, San Luis Obispo<br/>
+                                        </h6>
+                                        <p className="text-align-center" style={{fontSize: '12px', color: '#ffffff' }}>
+                                            <strong>2:45pm</strong> - 1st Shuttle from La Cuesta Inn<br/>
+                                            <strong>3:15pm</strong> - 2nd Shuttle from La Cuesta Inn<br/>
+                                            <strong>3:45pm </strong> - Please be seated promptly
+                                        </p>
                                     </>
                                     <br/>
                                     <>
@@ -90,21 +78,17 @@ export default function Events({locale, desktopAsset, enableYear, currentYear}) 
                                             <h2 style={{margin: 0}}>🥂</h2>
                                         </div>
                                         <h5 className="mb-0 text-white"><strong>Wedding Celebration</strong></h5>
-                                        {currentYear === enableYear ? (
-                                            <>
-                                                <h6 className="mb-5 text-white">4:30pm - 11:00pm @Flying Caballos</h6>
-                                                <p className="text-align-center">
-                                                    4:30pm Cocktail Hour | 5:30pm Dinner <br/>
-                                                    7:00pm Party | 9:00pm Snacks
-
-                                                </p>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <p className="text-align-center">More details to follow</p>
-                                            </>
-                                        )
-                                        }
+                                        <h6 className="mb-5 text-white" style={{fontSize: '12px', color: '#ffffff' }}>
+                                            <strong>When: </strong> 3:30PM - Flying Caballos Ranch <br/>
+                                            <strong>Where: </strong>1150 Farmhouse Ln, San Luis Obispo<br/>
+                                        </h6>
+                                        <p className="text-align-center" style={{fontSize: '12px', color: '#ffffff' }}>
+                                            <strong>4:30pm</strong> - Cocktail Hour & Appetizers <br/>
+                                            <strong>6:00pm</strong> - Dinner<br/>
+                                            <strong>7:30pm</strong> - Speeches & Pie<br/>
+                                            <strong>8:00pm </strong> - Party <br/>
+                                            <strong>10:00pm</strong> - Fireside S'mores
+                                        </p>
                                     </>
                                 </>
                             ) : locale === 'at' ? (
@@ -116,14 +100,14 @@ export default function Events({locale, desktopAsset, enableYear, currentYear}) 
                                             <h2 style={{margin: 0}}>💍</h2>
                                         </div>
                                         <h5 className="mb-0 text-white"><strong>Wedding Ceremony</strong></h5>
-                                            <h6 className="mb-5 text-white" style={{fontSize: '12px', color: '#ffffff' }}>
-                                                <strong>When: </strong> 3:00PM - Die Deantnerin <br/>
-                                                <strong>Where: </strong>Bürglalmweg 25, Dienten am Hochkönig<br/>
-                                            </h6>
-                                            <p className="text-align-center" style={{fontSize: '12px', color: '#ffffff' }}>
-                                                <strong>1:30pm</strong> - Shuttle from Hotel Eder, Maria Alm <br/>
-                                                <strong>2:45pm </strong> - Please be seated promptly
-                                                </p>
+                                        <h6 className="mb-5 text-white" style={{fontSize: '12px', color: '#ffffff' }}>
+                                            <strong>When: </strong> 3:00PM - Die Deantnerin <br/>
+                                            <strong>Where: </strong>Bürglalmweg 25, Dienten am Hochkönig<br/>
+                                        </h6>
+                                        <p className="text-align-center" style={{fontSize: '12px', color: '#ffffff' }}>
+                                            <strong>1:30pm</strong> - Shuttle from Hotel Eder, Maria Alm <br/>
+                                            <strong>2:45pm </strong> - Please be seated promptly
+                                        </p>
                                     </>
                                     <br/>
                                     <>
@@ -131,19 +115,19 @@ export default function Events({locale, desktopAsset, enableYear, currentYear}) 
                                             <h2 style={{margin: 0}}>🥂</h2>
                                         </div>
                                         <h5 className="mb-0 text-white"><strong>Wedding Celebration</strong></h5>
-                                            <h6 className="mb-5 text-white" style={{fontSize: '12px', color: '#ffffff' }}>
-                                                <strong>When: </strong> 3:30PM - Die Deantnerin <br/>
-                                                <strong>Where: </strong>Bürglalmweg 25, Dienten am Hochkönig<br/>
-                                            </h6>
-                                            <p className="text-align-center" style={{fontSize: '12px', color: '#ffffff' }}>
+                                        <h6 className="mb-5 text-white" style={{fontSize: '12px', color: '#ffffff' }}>
+                                            <strong>When: </strong> 3:30PM - Die Deantnerin <br/>
+                                            <strong>Where: </strong>Bürglalmweg 25, Dienten am Hochkönig<br/>
+                                        </h6>
+                                        <p className="text-align-center" style={{fontSize: '12px', color: '#ffffff' }}>
 
-                                                <strong>3:30pm</strong> - Cocktail Hour & Fingerfood <br/>
-                                                <strong>5:15pm</strong> - Cake <br/>
-                                                <strong>6:30pm</strong> - Seating & Speeches <br/>
-                                                <strong>7:00pm </strong> - Dinner <br/>
-                                                <strong>8:45pm</strong> - Party <br/>
-                                                <strong>10:30pm</strong> - Late night snacks
-                                            </p>
+                                            <strong>3:30pm</strong> - Cocktail Hour & Fingerfood <br/>
+                                            <strong>5:15pm</strong> - Cake <br/>
+                                            <strong>6:30pm</strong> - Seating & Speeches <br/>
+                                            <strong>7:00pm </strong> - Dinner <br/>
+                                            <strong>8:45pm</strong> - Party <br/>
+                                            <strong>10:30pm</strong> - Late night snacks
+                                        </p>
                                     </>
                                 </>
                             ) : null}
