5c258ad50cb3cdebe5e9511dbe1227896812ff47
cebrakadabra <admin@relax-tax.at>
2022-11-13 16:28:40 -0800

    austria schedule

--- components/Events.js
+++ components/Events.js
@@ -54,24 +54,15 @@ export default function Events({locale, desktopAsset, enableYear, currentYear}) 
                                             <h2 style={{margin: 0}}>🍕</h2>
                                         </div>
                                         <h5 className="mb-0"><strong>Welcome Dinner</strong></h5>
-                                        {currentYear === enableYear ? (
-                                            <>
-                                                <h6 className="mb-5">6:00pm @TBD</h6>
-                                                <p className="text-align-center">Please tell us if you can make it</p>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <p className="text-align-center">More details to follow</p>
-                                            </>
-                                        )
-                                        }
+                                            <h6 className="mb-5" style={{fontSize: '12px', color: '#787878', }}><strong>When:</strong> 6:00pm - Hotel Eder <br/><strong>Where:</strong> Am Dorfpl. 5, Maria Alm</h6>
+                                            <p className="text-align-center" style={{fontSize: '12px', color: '#787878', }}><strong>Who:</strong> Exclusively for the Bridal Party (Bridesmaids & Groomsmen) and for those who travel from far and close family members</p>
                                     </>
                                 </>
                             ) : null}
                         </div>
                     </div>
                     <div className="col-md-6 col-lg-4 d-flex ">
-                        <div className="card card-body justify-content-between bg-primary text-light">
+                        <div className="card card-body justify-content-between bg-primary text-light" style={{border: '0px'}}>
                             {locale === 'us' ? (
                                 <>
                                     <>
@@ -125,18 +116,14 @@ export default function Events({locale, desktopAsset, enableYear, currentYear}) 
                                             <h2 style={{margin: 0}}>💍</h2>
                                         </div>
                                         <h5 className="mb-0 text-white"><strong>Wedding Ceremony</strong></h5>
-                                        {currentYear === enableYear ? (
-                                            <>
-                                                <h6 className="mb-5 text-white">3:30PM @Die.Deantnerin</h6>
-                                                <p className="text-align-center">Please be seated by 04:00pm</p>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <p className="text-align-center">More details to follow</p>
-                                            </>
-                                        )
-                                        }
-
+                                            <h6 className="mb-5 text-white" style={{fontSize: '12px', color: '#ffffff' }}>
+                                                <strong>When: </strong> 3:00PM - Die Deantnerin <br/>
+                                                <strong>Where: </strong>Bürglalmweg 25, Dienten am Hochkönig<br/>
+                                            </h6>
+                                            <p className="text-align-center" style={{fontSize: '12px', color: '#ffffff' }}>
+                                                <strong>1:30pm</strong> - Shuttle from Hotel Eder, Maria Alm <br/>
+                                                <strong>2:45pm </strong> - Please be seated promptly
+                                                </p>
                                     </>
                                     <br/>
                                     <>
@@ -144,21 +131,19 @@ export default function Events({locale, desktopAsset, enableYear, currentYear}) 
                                             <h2 style={{margin: 0}}>🥂</h2>
                                         </div>
                                         <h5 className="mb-0 text-white"><strong>Wedding Celebration</strong></h5>
-                                        {currentYear === enableYear ? (
-                                            <>
-                                                <h6 className="mb-5 text-white">4:30pm - 2:00am @Die.Deantnerin</h6>
-                                                <p className="text-align-center">
-                                                    4:30pm Cocktail Hour | 5:30pm Dinner <br/>
-                                                    7:00pm Party | 12:00am Midnight Snack
+                                            <h6 className="mb-5 text-white" style={{fontSize: '12px', color: '#ffffff' }}>
+                                                <strong>When: </strong> 3:30PM - Die Deantnerin <br/>
+                                                <strong>Where: </strong>Bürglalmweg 25, Dienten am Hochkönig<br/>
+                                            </h6>
+                                            <p className="text-align-center" style={{fontSize: '12px', color: '#ffffff' }}>
 
-                                                </p>
-                                            </>
-                                        ) : (
-                                            <>
-                                                <p className="text-align-center">More details to follow</p>
-                                            </>
-                                        )
-                                        }
+                                                <strong>3:30pm</strong> - Cocktail Hour & Fingerfood <br/>
+                                                <strong>5:15pm</strong> - Cake <br/>
+                                                <strong>6:30pm</strong> - Seating & Speeches <br/>
+                                                <strong>7:00pm </strong> - Dinner <br/>
+                                                <strong>8:45pm</strong> - Party <br/>
+                                                <strong>10:30pm</strong> - Late night snacks
+                                            </p>
                                     </>
                                 </>
                             ) : null}
